3843e773108c635ea442099a87bf21bc53c7bc9c
jhods16 <jessicarhodson@gmail.com>
2018-12-07 20:37:42 -0800

    Work on adding username to session object

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   // if there are no cookies on the request
+  console.log('inside create session', req.cookies, req.get('Cookie'));
   if (!req.get('Cookie')) {
   //  models.Sessions.create()
     models.Sessions.create()
@@ -18,13 +19,32 @@ module.exports.createSession = (req, res, next) => {
         };
         next();
       });
-  }
-  //  then res.cookie = { shortlyid: hash }
   // else
-  // if there is a cookie on the request
-  //  check that there is a hash in the sessions table to match cookie
-  //  if there is grab the userID associated with it
-  //    create a sessions object {hash, userID, username} and assign it to req.session
+  } else {
+    console.log('inside else', req.cookies, req.get('Cookie'));
+//  check that there is a hash in the sessions table to match cookie
+    models.Sessions.get({hash: req.cookies['shortlyid']})
+      .then(result => {
+        console.log('tried to get', result);
+        req.session = {
+          hash: result.hash,
+          userId: result.userId
+        };
+        return result.userId;
+      })
+      .then((userId) => {
+        console.log('userId?', userId);
+        return models.Users.get({id: userId});
+      })
+      .then(result => {
+        console.log(result);
+        req.session.user = {username: result.username};
+        next();
+      });
+    
+  }
+  //  if there is grab the userId associated with it
+  //    create a sessions object {hash, userId, username} and assign it to req.session
   //  if there is not a matching cookie
   //    delete the cookie and create a new one
   //    models.Sessions.create()
