eeb66e483e2533bfeb676e66bc42306a32e8e05e
jhods16 <jessicarhodson@gmail.com>
2018-12-07 16:19:40 -0800

    Route to login, signup pages and implement create user

--- server/app.js
+++ server/app.js
@@ -18,67 +18,109 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
 
 app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
         throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+app.get('/login', 
+  (req, res) => {
+    res.render('login');
+  });
 
+app.post('/login', 
+  (req, res) => {
+    
+  });
+
+app.get('/signup', 
+  (req, res) => {
+    res.render('signup');
+  });
+
+app.post('/signup', 
+  (req, res) => {
+    //get username
+    var username = req.body.username;
+    console.log('got username:', username);
+    //get password
+    var password = req.body.password;
+    console.log('got password:', password);
+    //get cookie from header?
+    //pass these values along to models.Users.create
+    models.Users.create({ username, password })
+      .then(() => {
+        // console.log('created');
+        // res.sendStatus(201);
+        res.redirect('/');
+      })
+      .catch((err) => {
+        // console.log('error');
+        res.redirect('/signup');
+        // res.status(400).send(err);
+      });
+    //  if err
+    //    redirect back to signup -- user already exists
+    //  else
+    //    respond with 201, headers -- including cookie?
+    //    respond with any other info?
+    //    redirect to index -- in the future we will need to do something re the session
+  });
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
