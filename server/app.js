808dafba70cbcf0645c493b005be6b19c66a1824
jhods16 <jessicarhodson@gmail.com>
2018-12-08 15:01:00 -0800

    Implement privileged access for '/', '/create', and '/links'

--- server/app.js
+++ server/app.js
@@ -17,62 +17,81 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(cookieParser);
 app.use(Auth.createSession);
 
+const verifySession = (req) => {
+  return models.Sessions.isLoggedIn(req.session);
+};
 
 app.get('/', 
   (req, res) => {
-    res.render('index');
+    if (verifySession(req)) {
+      res.render('index');
+    } else {
+      res.redirect('/login');
+    }
   });
 
 app.get('/create', 
   (req, res) => {
-    res.render('index');
+    if (verifySession(req)) {
+      res.render('index');
+    } else {
+      res.redirect('/login');
+    }
   });
 
 app.get('/links', 
   (req, res, next) => {
-    models.Links.getAll()
-      .then(links => {
-        res.status(200).send(links);
-      })
-      .error(error => {
-        res.status(500).send(error);
-      });
+    if (verifySession(req)) {
+      models.Links.getAll()
+        .then(links => {
+          res.status(200).send(links);
+        })
+        .error(error => {
+          res.status(500).send(error);
+        });
+    } else {
+      res.redirect('/login');
+    }
   });
 
 app.post('/links', 
   (req, res, next) => {
-    var url = req.body.url;
-    if (!models.Links.isValidUrl(url)) {
-      // send back a 404 if link is not valid
-      return res.sendStatus(404);
-    }
+    if (verifySession(req)) {
+      var url = req.body.url;
+      if (!models.Links.isValidUrl(url)) {
+        // send back a 404 if link is not valid
+        return res.sendStatus(404);
+      }
 
-    return models.Links.get({ url })
-      .then(link => {
-        if (link) {
+      return models.Links.get({ url })
+        .then(link => {
+          if (link) {
+            throw link;
+          }
+          return models.Links.getUrlTitle(url);
+        })
+        .then(title => {
+          return models.Links.create({
+            url: url,
+            title: title,
+            baseUrl: req.headers.origin
+          });
+        })
+        .then(results => {
+          return models.Links.get({ id: results.insertId });
+        })
+        .then(link => {
           throw link;
-        }
-        return models.Links.getUrlTitle(url);
-      })
-      .then(title => {
-        return models.Links.create({
-          url: url,
-          title: title,
-          baseUrl: req.headers.origin
+        })
+        .error(error => {
+          res.status(500).send(error);
+        })
+        .catch(link => {
+          res.status(200).send(link);
         });
-      })
-      .then(results => {
-        return models.Links.get({ id: results.insertId });
-      })
-      .then(link => {
-        throw link;
-      })
-      .error(error => {
-        res.status(500).send(error);
-      })
-      .catch(link => {
-        res.status(200).send(link);
-      });
+    } else {
+      res.redirect('/login');
+    }
   });
 
 /************************************************************/
