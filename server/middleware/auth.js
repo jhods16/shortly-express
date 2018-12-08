233d2d8b5cb69d9546f9925164bab9eea84ec9bc
jhods16 <jessicarhodson@gmail.com>
2018-12-08 10:51:28 -0800

    Assign username and userId to session if session exists

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const Promise = require('bluebird');
 module.exports.createSession = (req, res, next) => {
   // if there are no cookies on the request
   console.log('inside create session', req.cookies, req.get('Cookie'));
-  if (!req.get('Cookie')) {
+  if (!req.cookies || Object.keys(req.cookies).length === 0) {
   //  models.Sessions.create()
     models.Sessions.create()
       .then(result => {
@@ -22,7 +22,7 @@ module.exports.createSession = (req, res, next) => {
   // else
   } else {
     console.log('inside else', req.cookies, req.get('Cookie'));
-//  check that there is a hash in the sessions table to match cookie
+    //  check that there is a hash in the sessions table to match cookie
     models.Sessions.get({hash: req.cookies['shortlyid']})
       .then(result => {
         console.log('tried to get', result);
@@ -34,6 +34,9 @@ module.exports.createSession = (req, res, next) => {
       })
       .then((userId) => {
         console.log('userId?', userId);
+        if (userId === null) {
+          next();
+        }
         return models.Users.get({id: userId});
       })
       .then(result => {
